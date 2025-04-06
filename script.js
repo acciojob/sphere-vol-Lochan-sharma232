a5f50ce3ced1e61ac87ea56239133c30df15b3e9
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 21:40:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function volume_sphere() {
+	let height=parseFloat(document.getElementById("height").value);
+	let radius=parseFloat(document.getElementById("radius").value);
+
+	let volume=Math.PI*radius**2**height;
+	document.getElementById('Calculate').innerHTML=volume;
     //Write your code here
 } 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
