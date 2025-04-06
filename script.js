0c9ffd39d3e0d075acae7357f9a7dd4af7f8ccaa
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-06 22:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,4 +8,4 @@ function volume_sphere() {
 	
     //Write your code here
 } 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+//window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
